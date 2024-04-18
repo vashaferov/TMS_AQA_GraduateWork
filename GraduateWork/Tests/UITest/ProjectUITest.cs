bd805f16c280668c8874172aa5f6ad61309e17ed
vashaferov <jonirz55@gmail.com>
2024-04-18 17:51:00 +0500

    Обновленный ProjectUITest.cs

--- GraduateWork/Tests/UITest/ProjectUITest.cs
+++ GraduateWork/Tests/UITest/ProjectUITest.cs
@@ -7,19 +7,34 @@ namespace GraduateWork.Tests.UITest;
 [AllureSuite("UI Project Tests")]
 public class ProjectUITest : BaseTest
 {
+    string projectName = $"Project 1";
+    // string projectName = $"Project {DateTime.Now}";
+    
     [Test]
+    [Order(1)]
     [AllureName("Создание новго проекта")]
     [AllureDescription("Тест на создание сущности")]
     public void CreateProjectTest()
     {
-        string projectName = $"Project {DateTime.Now}";
-        
         LoginSteps.NavigateToLoginPage();
         LoginSteps.SuccessfulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password);
 
-        Assert.That(DashboardSteps.CreateProject(projectName));
-        
+        Assert.That(ProjectSteps.CreateProject(projectName));
+
         Assert.That(LoginSteps.DashboardPage.DashbordProjectName.Text, Is.EqualTo(projectName));
         AllureApi.Step($"Проект \"{projectName}\" успешно создан");
+    }
+
+    [Test]
+    [Order(2)]
+    [AllureName("Удаление проекта")]
+    [AllureDescription("Тест на удаление сущности\nТест на отображения диалогового окна")]
+    public void DeletProjectTest()
+    {
+        LoginSteps.NavigateToLoginPage();
+        LoginSteps.SuccessfulLogin(Configurator.AppSettings.Username, Configurator.AppSettings.Password);
+
+        Assert.That(ProjectSteps.DeleteProject(projectName));
+        AllureApi.Step($"Проект \"{projectName}\" отсутствует в списке");
     }
 }
